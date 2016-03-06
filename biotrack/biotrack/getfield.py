1b5c0181d26205b27db27a4f0c9f7f857c3a1aa1
Dan Boo <daniel.boocock@protonmail.ch>
2016-03-06 18:16:14 +0000

    Outline a new Model class for holding components and UniProt entries

--- biotrack/biotrack/getfield.py
+++ biotrack/biotrack/getfield.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 """Retrieve a specified field given a UniProt accession"""
+import Bio
+
+from model import Model
+
+
+
+
+
+# Test out UniSaves's RESTful Web-Services on ribR in B. subtilis
+# http://www.ebi.ac.uk/uniprot/unisave/rest/raw/Q00001/79
 
 # Find the given UniProtKB-AC in a version of the uniprot database.
 # Retrieve a field from the accession, or if it does not exist
---
+++ biotrack/biotrack/model.py
@@ -0,0 +1,45 @@
+"""Class for retrieving and comparing new and old model components."""
+import Bio
+
+
+# Could potentially hold the entries in a biopython object, presumably
+# one exists, rather than writing to file.
+# Could probably also retreive only the relevent field rather than
+# doing all of the initial setup.
+class Model(object):
+    """Class containing model components, versions, and accessions."""
+    def __init__(self, new_filename, old_filename):
+        # self.components = self.parse_accessions(filename)
+        # self.new_entries = self.fetch_new_entries(new_filename)
+        # self.old_entries = self.fetch_old_entries(old_filename)
+        pass
+
+    
+    def parse_accessions(self, filename):
+        """Read in a list of UniProt accessions with entry versions."""
+        pass
+
+
+    def fetch_old_entries(self, filename):
+        """Obtain the past UniProt entries of model components.
+
+        Write these to file for later comparison.
+        """
+        # with open(filename, 'a') as f:
+        pass
+
+
+    def fetch_new_entries(self, filename):
+        """Obtain the present UniProt entries of model components.
+
+        Write these to file for later comparison.
+        """
+        # with open(filename, 'a') as f:
+        pass
+
+    
+    def compare_entries(self, field):
+        """Find differeces in a field between new and old entries."""
+        pass
+
+
