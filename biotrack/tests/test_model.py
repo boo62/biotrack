0f76387fbfced977bcbcf7c8acf7b9b759ef2583
Dan Boo <daniel.boocock@protonmail.ch>
2016-03-07 09:12:48 +0000

    Modified path finding in Model tests

--- biotrack/tests/test_model.py
+++ biotrack/tests/test_model.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python
 import unittest
+import os
 
 from biotrack.model import Model
 
 
 
-class TestSimpleModel(unittest.TestCase):
+class TestBasicTwoComponentModel(unittest.TestCase):
 
     def setUp(self):
-        self.model_1 = Model("tests/example_models/two_components.csv")
+        """Setup simple model from two component test case."""
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+        self.model_1 = Model(test_dir + "/example_models/two_components.csv")
 
     def tearDown(self):
         self.model_1 = None
 
 
     def test_component_lengths(self):
+        """Test component lenghts.
+
+        The correct number of components should be read from file and
+        each component should have length two.
+
+        """
         self.assertEqual(len(self.model_1.components), 2)
         for component in self.model_1.components:
             self.assertEqual(len(component), 2)
 
 
     def test_component_types(self):
+        """Test that Model.components have correct type and format.
+
+        The accession of each component should match a UniProt
+        accession and the version should convert to an integer.
+
+        """
         uniprot_accession = "[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}"
         for component in self.model_1.components:
             self.assertIsInstance(component, tuple)
@@ -29,7 +44,6 @@ class TestSimpleModel(unittest.TestCase):
             self.assertRegexpMatches(component[0], uniprot_accession)
             self.assertIsInstance(int(component[1]), int)
 
-        
             
     def test_accession_parsing_fail_when_not_accession(self):
         pass
