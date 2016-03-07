07978b31fae65cf9c57fc96688ef37947d192204
Dan Boo <daniel.boocock@protonmail.ch>
2016-03-07 15:10:42 +0000

    Add a comparison to Component object

--- biotrack/biotrack/component.py
+++ biotrack/biotrack/component.py
@@ -9,7 +9,28 @@ class Component(object):
         self.old_entry = None
         self.new_entry = None
 
-    def __eq__(self):
-        """Two components are equal if their accessions are equivalent."""
-        pass
+    def __eq__(self, comp2):
+        """Two components are equal if their accessions are
+        equivalent."""
+        # There a several more permutaions here. Not sure if it is
+        # worth doing them all
+        if self.new_entry is not None and comp2.new_entry is not None:
+            return self.accession in comp2.new_entry.accessions
+        else:
+            raise ValueError("Not comparing from new lists of accessions.")
+            return self.accession == comp2.accession
+
+
+    def __str__(self):
+        """Return string representation of Component object.
+
+        If the component has old and new UniProt entries, those fields
+        are True.
+        """
+        template ="Component: Acc. {}, Ver. {}, Old = {}, New = {}."
+        has_old = (self.old_entry is not None)
+        has_new = (self.new_entry is not None)
+        return template.format(self.accession, self.version,
+                               has_old, has_new) 
+            
         
