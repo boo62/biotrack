d20cee4045fddc1c2e4af372d2de3cecb1eb4575
Dan Boo <daniel.boocock@protonmail.ch>
2016-03-08 15:25:46 +0000

    Add subtract method for Fields object

--- biotrack/fields.py
+++ biotrack/fields.py
@@ -8,16 +8,18 @@ import re
 class Fields(object):
 
     # get comments into a dictionary.
-    def __init__(self, comments):
+    def __init__(self, comments=None):
         # A set of fields so can do comparisions using union and
         # intersection. Can then give new and old Comments attributes
         # to Components.
         # Fields as a dictionary (or nested dictionary).
         # split(s, sep=":", maxsplit=1)
-        fields = self.parse_comments(comments)
-        self.field_set = fields[0]
-        self.field_dict = fields[1]
+        if comments is not None:
+            fields = self.parse_comments(comments)
+            self.field_set = fields[0]
+            self.field_dict = fields[1]
 
+            
     def parse_comments(self, comments):
         # Keep fields as uppercase in case we want to compare back with
         # UniProt.Record.comments
@@ -33,3 +35,36 @@ class Fields(object):
         sets_equal = (self.field_set == fields2.field_set)
         dicts_equal = (self.field_dict == fields2.field_dict)
         return sets_equal and dicts_equal
+
+    
+    # Subtract to Fields objects.
+    def __sub__(self, other):
+        """Return a Fields object containing differences.
+
+        Differences are new fields or changed field values but
+        not removed fields. If there are no differnces returns None.
+        """
+        # Do I need to check if other is a Field or let it fail?
+        if self == other:
+            return None
+        else:
+            # Need to test not only if fields are new but if fields
+            # have changed. Does not test removed fields.
+            # Find new fields.
+            new = self.field_set - other.field_set
+            # Find common fields whose values have changed.
+            common = self.field_set & other.field_set
+            changed = [field for field in common if
+                       self.field_dict[field] != other.field_dict[field]]
+            # Attributes for Fields object containing the changes
+            new_field_set = set(changed) | new
+            new_field_dict = {k: self.field_dict[k] for k in new_field_set}
+            # Create Fields object containing the changes.
+            dif = Fields()
+            dif.field_set = new_field_set
+            dif.field_dict = new_field_dict
+            return dif
+
+    def __str__(self):
+        pass
+        
